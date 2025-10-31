3483df5da92beb900f5102b2f323c36f54a57e88
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-31 09:23:04 +0700

    menambah kolom

---
+++ app/src/main/java/com/example/project02/TeksEdit.kt
@@ -0,0 +1,60 @@
+package com.example.project02
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormDataDiri(modifier: Modifier) {
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember {mutableStateOf("")}
+    var textAlamat by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
+
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember {mutableStateOf("")}
+    var alamat by remember {mutableStateOf("")}
+    var jenis by remember {mutableStateOf("")}
+
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                val it = null
+                textNama = it
+            }
+    )
+
+
+
+
+}
+
+@Composable
+fun Column(
+    modifier: padding,
+    verticalArrangement: Top,
+    horizontalAlignment: CenterHorizontally,
+    content: @Composable () -> OutlinedTextField
+) {
+    TODO("Not yet implemented")
+}
