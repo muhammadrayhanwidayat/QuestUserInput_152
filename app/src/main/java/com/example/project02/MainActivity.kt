d359a74e3ccc4867fec29b3200ee707545ad598e
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-31 10:32:00 +0700

    tambahkan }

--- app/src/main/java/com/example/project02/MainActivity.kt
+++ app/src/main/java/com/example/project02/MainActivity.kt
@@ -18,4 +18,5 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     FormDataDiri(modifier = Modifier)
+                }
 
