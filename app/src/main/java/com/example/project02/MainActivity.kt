a5f89b36c91f17b0bc17e5382c3f59dd00720d88
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-31 10:30:54 +0700

    set material theme

--- app/src/main/java/com/example/project02/MainActivity.kt
+++ app/src/main/java/com/example/project02/MainActivity.kt
@@ -11,4 +11,9 @@ import androidx.compose.ui.Modifier
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
 
